b17d58edbba5eef13bb3444d7649342121228639
techiemow <mowriyaabk@gmail.com>
2024-08-10 13:18:55 +0530

    backend created

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-require("dotenv").config();
+
 
 const app = express();
 app.use(cors());
